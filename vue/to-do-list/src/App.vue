1c792ae6486d4ddec95a3b8adedf47d79ba9d9ba
fatihylm <fatihyilmaz517@gmail.com>
2023-01-18 09:50:36 +0100

    to do list

---
+++ vue/to-do-list/src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <todo-list></todo-list>
+</template>
+
+<script>
+import TodoList from './components/todoList.vue';
+
+export default {
+  name: "App",
+  components: {
+    TodoList,
+  },
+};
+</script>
---
+++ vue/to-do-list/src/components/todoList.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">{{ task.text }} <button @click="removeTask(task.id)">X</button></li>
+    </ul>
+    <input v-model="newTaskText" @keyup.enter="addTask" placeholder="Add a task">
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tasks: [],
+      newTaskText: ''
+    }
+  },
+  methods: {
+    addTask() {
+      this.tasks.push({
+        id: Date.now(),
+        text: this.newTaskText
+      })
+      this.newTaskText = ''
+    },
+    removeTask(id) {
+      this.tasks = this.tasks.filter(task => task.id !== id)
+    }
+  }
+}
+</script>
